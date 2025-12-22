5a9ac39acab50de3e04384cb24d95f46e4d266a7
Max Bolsunovsky <jamax.cherry@gmail.com>
2025-12-22 11:07:46 +0200

    Edited MainPage (deleted unused imports)

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,5 @@
-import { useAuth } from '@/hooks/useAuth';
-import { Button } from '@/components/ui/button';
-
 export default function MainPage() {
-  const { logout } = useAuth();
-	
+
 	return (
 		<>
 			Main Page
